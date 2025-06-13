2a7796a24d9166aeae4462bd2caaa992787d7c5b
ArezkiCherfouh <skileralok422@gmail.com>
2025-06-13 20:58:08 +0100

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],
+)
+WEATHER_API_KEY = "005806397292f0dbe2c0bf815795e7fb"
+@app.get("/weather")
+def get_weather(city: str): 
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}&units=metric"
+    res = requests.get(url) 
+    data = res.json()  
+    if res.status_code == 200:
+        return {
+            "city": data["name"],
+            "temperature": data["main"]["temp"],
+            "feels_like" : data["main"]["feels_like"],
+            "min_temp" : data["main"]["temp_min"],
+            "max_temp" : data["main"]["temp_max"],
+            "description": data["weather"][0]["description"].title()
+        }
+    else:
+        return {"error": f"{res.status_code} City not found"}
